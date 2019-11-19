30faf8be9165eef26c6b98bb0cb417bbe8194058
Tamal Saha <tamal@appscode.com>
2019-11-19 01:18:58 -0800

    Remove dependency on k8s.io/apiserver/pkg/util/logs xref: https://github.com/kubernetes/kubernetes/issues/74560
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -75,7 +75,6 @@ import (
 	restclient "k8s.io/client-go/rest"
 	basecompatibility "k8s.io/component-base/compatibility"
 	"k8s.io/component-base/featuregate"
-	"k8s.io/component-base/logs"
 	"k8s.io/component-base/metrics/features"
 	"k8s.io/component-base/metrics/prometheus/slis"
 	"k8s.io/component-base/tracing"
@@ -1111,11 +1110,11 @@ func installAPI(name string, s *GenericAPIServer, c *Config) {
 			goruntime.SetBlockProfileRate(1)
 		}
 		// so far, only logging related endpoints are considered valid to add for these debug flags.
-		routes.DebugFlags{}.Install(s.Handler.NonGoRestfulMux, "v", routes.StringFlagPutHandler(logs.GlogSetter))
+		routes.DebugFlags{}.Install(s.Handler.NonGoRestfulMux, "v", routes.StringFlagPutHandler(glogSetter))
 	}
 	if s.UnprotectedDebugSocket != nil {
 		s.UnprotectedDebugSocket.InstallProfiling()
-		s.UnprotectedDebugSocket.InstallDebugFlag("v", routes.StringFlagPutHandler(logs.GlogSetter))
+		s.UnprotectedDebugSocket.InstallDebugFlag("v", routes.StringFlagPutHandler(glogSetter))
 		if c.EnableContentionProfiling {
 			goruntime.SetBlockProfileRate(1)
 		}
@@ -1209,4 +1208,13 @@ func SetHostnameFuncForTests(name string) {
 		err = nil
 		return
 	}
+}
+
+// glogSetter is a setter to set glog level.
+func glogSetter(val string) (string, error) {
+	var level klog.Level
+	if err := level.Set(val); err != nil {
+		return "", fmt.Errorf("failed set klog.logging.verbosity %s: %v", val, err)
+	}
+	return fmt.Sprintf("successfully set klog.logging.verbosity to %s", val), nil
 }
